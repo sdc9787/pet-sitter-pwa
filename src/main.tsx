b56f23aa80dcc6ed37c11801032f95eb2786ac51
sdc9787 <106896972+sdc9787@users.noreply.github.com>
2024-02-06 21:59:49 +0900

    11

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import "./styles/index.css";
-import ReactDOM from "react-dom/client";
+import * as ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import store from "./store/store.tsx";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import React from "react";
+import * as React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
