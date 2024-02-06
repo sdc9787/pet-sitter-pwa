96d18553445675394594cc8a7927b055ae149050
sdc9787 <106896972+sdc9787@users.noreply.github.com>
2024-02-06 19:53:24 +0900

    tabbar

---
+++ src/Component/TabBar.tsx
@@ -0,0 +1,50 @@
+import "../CSS/TabBar.css";
+type TabBarProps = {
+  tabState: number;
+  setTabState: React.Dispatch<React.SetStateAction<number>>;
+};
+
+function TabBar(props: TabBarProps) {
+  let tabbarIconFalse = ["xi-home-o", "xi-paper-o", "xi-map-o", "xi-forum-o", "xi-user-o"];
+  let tabbarIconTrue = ["xi-home", "xi-paper", "xi-map", "xi-forum", "xi-user"];
+  let tabbarIconName = ["홈", "산책", "지도", "커뮤니티", "설정"];
+
+  return (
+    <>
+      <div className="tabbar">
+        {tabbarIconTrue.map((a, i) => {
+          return (
+            <div
+              key={i}
+              onClick={() => {
+                props.setTabState(i);
+              }}
+              className="tabbar-element">
+              <i className={props.tabState === i ? a + " xi-2x" : tabbarIconFalse[i] + " xi-2x"}></i>
+              <span className="tabbar-element-name">{tabbarIconName[i]}</span>
+            </div>
+          );
+        })}
+
+        {/* <div className="tabbar-element">
+          <i className="xi-paper-o xi-2x"></i>
+          <span className="tabbar-element-name">산책</span>
+        </div>
+        <div className="tabbar-element">
+          <i className="xi-map-o xi-2x"></i>
+          <span className="tabbar-element-name">지도</span>
+        </div>
+        <div className="tabbar-element">
+          <i className="xi-forum-o xi-2x"></i>
+          <span className="tabbar-element-name">커뮤니티</span>
+        </div>
+        <div className="tabbar-element">
+          <i className="xi-user-o xi-2x"></i>
+          <span className="tabbar-element-name">MY</span>
+        </div> */}
+      </div>
+    </>
+  );
+}
+
+export default TabBar;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./CSS/index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import store from "./store/store.tsx";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  //<React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+  //</React.StrictMode>
+);
