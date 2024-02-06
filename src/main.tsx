646380a59e54e74285505a987a71eb2f92168452
sdc9787 <106896972+sdc9787@users.noreply.github.com>
2024-02-06 19:59:27 +0900

    tabbar

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,14 @@ import App from "./App.tsx";
 import store from "./store/store.tsx";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  //<React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-  //</React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
