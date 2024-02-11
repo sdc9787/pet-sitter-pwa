cf85bb9dbcd0258bff25583bdb1ec835e2f05f45
sdc9787 <106896972+sdc9787@users.noreply.github.com>
2024-02-11 20:16:55 +0900

    1

--- src/Routes/setting.tsx
+++ src/Routes/setting.tsx
@@ -14,7 +14,7 @@ function Setting() {
             <img className="setting-profile-icon" src={profileImg} alt="프로필 아이콘" />
             <span className="setting-profile-name">{profileName}</span>
           </div>
-          <div className="setting-profile-set">프로필 변경</div>
+          <button className="setting-profile-set">프로필 변경</button>
         </div>
       </div>
     </>
