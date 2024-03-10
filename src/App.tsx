97dd69cd0444a20dd91a9a0ef198d1afd2c87e3f
sdc9787 <106896972+sdc9787@users.noreply.github.com>
2024-03-10 19:26:23 +0900

    1

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Pet from "./Routes/pet";
 import Map from "./Routes/map";
 import Community from "./Routes/community";
 import Login from "./Routes/login";
+import KakaoRedirect from "./Routes/oauth/callback/kakao/kakaoRedirect";
 
 function App() {
   let [tabState, setTabState] = useState<number>(() => JSON.parse(window.localStorage.getItem("tabState") as string) || 0); //class 체크 저장
@@ -22,6 +23,7 @@ function App() {
           <Route path="/map" element={<Map></Map>}></Route>
           <Route path="/community" element={<Community></Community>}></Route>
           <Route path="/setting" element={<Setting></Setting>}></Route>
+          <Route path="/oauth/callback/kakao" element={<KakaoRedirect></KakaoRedirect>}></Route>
           <Route path="*" element={<div>접근할 수 없는 페이지입니다.</div>}></Route>
         </Routes>
       </div>
