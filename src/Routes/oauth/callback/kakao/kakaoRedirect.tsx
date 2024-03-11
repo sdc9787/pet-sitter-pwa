e2a83eb25cd36bc9e4539aa2e5fe37118408dff1
sdc9787 <106896972+sdc9787@users.noreply.github.com>
2024-03-11 14:25:25 +0900

    test

--- src/Routes/oauth/callback/kakao/kakaoRedirect.tsx
+++ src/Routes/oauth/callback/kakao/kakaoRedirect.tsx
@@ -1,25 +1,24 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-function KakaoRedirect() {
+const Redirection = () => {
   const code = window.location.search;
   const navigate = useNavigate();
 
   useEffect(() => {
     console.log(import.meta.env.VITE_APP_API_URL);
-    console.log(code);
-    // axios.post(`${import.meta.env.VITE_APP_API_URL}`).then((r) => {
-    //   console.log(r.data);
+    axios.get(`${import.meta.env.VITE_APP_API_URL}${code}`).then((r) => {
+      console.log(r.data);
 
-    //   // 토큰을 받아서 localStorage같은 곳에 저장하는 코드를 여기에 쓴다.
-    //   localStorage.setItem("name", r.data.user_name); // 일단 이름만 저장했다.
+      // 토큰을 받아서 localStorage같은 곳에 저장하는 코드를 여기에 쓴다.
+      localStorage.setItem("name", r.data.user_name); // 일단 이름만 저장했다.
 
-    //   navigate("/loginSuccess");
-    // });
+      navigate("/loginSuccess");
+    });
   }, []);
 
-  return <div>로그인 중입니다...</div>;
-}
+  return <div>로그인 중입니다.</div>;
+};
 
-export default KakaoRedirect;
+export default Redirection;
