80a6bce9d3a9eef4e9d568a9ed5c49218ef62569
sdc9787 <106896972+sdc9787@users.noreply.github.com>
2024-03-11 20:26:50 +0900

    1

--- src/Routes/oauth/callback/kakao/kakaoRedirect.tsx
+++ src/Routes/oauth/callback/kakao/kakaoRedirect.tsx
@@ -5,8 +5,9 @@ import { useNavigate } from "react-router-dom";
 const Redirection = () => {
   const code = window.location.search;
   const navigate = useNavigate();
-
+  console.log(1);
   useEffect(() => {
+    console.log(1);
     console.log(import.meta.env.VITE_APP_API_URL);
     console.log(`${import.meta.env.VITE_APP_API_URL}${code}`);
     axios.get(`${import.meta.env.VITE_APP_API_URL}${code}`).then((r) => {
@@ -14,8 +15,6 @@ const Redirection = () => {
 
       // 토큰을 받아서 localStorage같은 곳에 저장하는 코드를 여기에 쓴다.
       localStorage.setItem("name", r.data.user_name); // 일단 이름만 저장했다.
-
-      navigate("/loginSuccess");
     });
   }, []);
 
