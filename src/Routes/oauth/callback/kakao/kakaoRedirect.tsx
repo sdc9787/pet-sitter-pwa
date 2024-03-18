77499bb641c94d615c339e1d7f02b04a203006f0
sdc9787 <106896972+sdc9787@users.noreply.github.com>
2024-03-18 09:15:36 +0900

    1

--- src/Routes/oauth/callback/kakao/kakaoRedirect.tsx
+++ src/Routes/oauth/callback/kakao/kakaoRedirect.tsx
@@ -5,14 +5,9 @@ import { useNavigate } from "react-router-dom";
 const Redirection = () => {
   const code = window.location.search;
   const navigate = useNavigate();
-  console.log(1);
   useEffect(() => {
-    console.log(1);
-    console.log(import.meta.env.VITE_APP_API_URL);
-    console.log(`${import.meta.env.VITE_APP_API_URL}${code}`);
     axios.get(`${import.meta.env.VITE_APP_API_URL}${code}`).then((r: any) => {
       console.log(r.data);
-
       window.localStorage.setItem("access_token", r.data.access_token); // 엑세스 토큰 저장
       window.localStorage.setItem("nick_name", r.data.nick_name); // 닉네임 저장
       window.localStorage.setItem("expires_at_unix", r.data.expires_at_unix); // 만료 시간 저장
