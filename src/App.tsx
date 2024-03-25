c73cb85e088aaa85fcde57226e92d81e9efdd179
sdc9787 <106896972+sdc9787@users.noreply.github.com>
2024-03-25 09:03:37 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,9 @@ function App() {
   //     navigate("/home");
   //   }
   // }, []);
-  navigate("/signup");
+  useEffect(() => {
+    navigate("/signup");
+  }, []);
   //tabState가 바뀔때마다 토큰 유효성 검사
   const checkTokenValidity = async () => {
     const expiresAtUnix = JSON.parse(window.localStorage.getItem("expires_at_unix") as string);
