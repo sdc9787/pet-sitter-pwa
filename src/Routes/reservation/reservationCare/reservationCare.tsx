522db1339202de8aaadd9868e965ecaf4b8c4ee9
sdc9787 <106896972+sdc9787@users.noreply.github.com>
2024-05-18 22:50:47 +0900

    test

--- src/Routes/reservation/reservationCare/reservationCare.tsx
+++ src/Routes/reservation/reservationCare/reservationCare.tsx
@@ -1,22 +1,7 @@
-import useGeolocation from "../../../hook/useGeolocation/useGeolocation";
-
 function ReservationCare() {
-  const { latitude, longitude, address, error } = useGeolocation();
-
   return (
     <div>
-      <h1>현재 위치 정보</h1>
-      {error ? (
-        <p>{error}</p>
-      ) : latitude && longitude ? (
-        <div>
-          <p>위도: {latitude}</p>
-          <p>경도: {longitude}</p>
-          <p>도로명 주소: {address}</p>
-        </div>
-      ) : (
-        <p>위치 정보를 가져오는 중...</p>
-      )}
+      <div>ReservationCare</div>
     </div>
   );
 }
