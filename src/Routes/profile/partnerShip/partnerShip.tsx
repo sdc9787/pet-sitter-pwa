8c0c8a23febe1f773904ebc1aa14377043f5880b
sdc9787 <106896972+sdc9787@users.noreply.github.com>
2024-06-03 20:37:09 +0900

    test

--- src/Routes/profile/partnerShip/partnerShip.tsx
+++ src/Routes/profile/partnerShip/partnerShip.tsx
@@ -1,9 +1,37 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import PartnerShipStep1 from "./partnerShipStep1";
 import PartnerShipStep2 from "./partnerShipStep2";
 import PartnerShipStep3 from "./partnerShipStep3";
+import { useEffect } from "react";
+import instanceJson from "../../../Component/axios/axiosJson";
+import { useAlert } from "../../../hook/useAlert/useAlert";
 
 function PartnerShip() {
+  const alertBox = useAlert();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    instanceJson
+      .get("/mypage/partner/apply/check")
+      .then((res) => {
+        console.log(res.data.partnerStep);
+        if (res.data.partnerStep === 0) {
+          navigate("/profile/partnerShip/step1");
+        } else if (res.data.partnerStep === 1) {
+          navigate("/profile/partnerShip/step2");
+        } else if (res.data.partnerStep === 2) {
+          navigate("/profile/partnerShip/step3");
+        } else if (res.data.partnerStep === 3) {
+          if (res.data.testCount == 3) {
+            alertBox("3회 불합격 하셨습니다, 다음에 다시 도전해주세요");
+          }
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
       <Routes>
