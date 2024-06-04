5ec730baa7b0c39483858198e5da733334a89b21
sdc9787 <106896972+sdc9787@users.noreply.github.com>
2024-06-04 09:15:47 +0900

    test

--- src/Component/camera/camera.jsx
+++ src/Component/camera/camera.jsx
@@ -13,10 +13,14 @@ const CameraComponent = ({ onCapture }) => {
   };
 
   return (
-    <div className="w-full h-auto">
-      <Camera ref={camera} aspectRatio={16 / 9} facingMode="environment" />
+    <>
+      <div className="w-full h-auto relative">
+        <Camera ref={camera} aspectRatio={16 / 9} facingMode="environment" />
+        <div className="absolute left-0 top-0 bottom-0 w-5 border-l-2 border-y-2 border-white"></div>
+        <div className="absolute right-0 top-0 bottom-0 w-5 border-r-2 border-y-2 border-white"></div>
+      </div>
       <button onClick={handleTakePhoto}>Take Photo</button>
-    </div>
+    </>
   );
 };
 
