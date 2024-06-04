d50d97d1120d478217eafd5fff5a3b8baacc1fd0
sdc9787 <106896972+sdc9787@users.noreply.github.com>
2024-06-04 10:07:47 +0900

    test

--- src/Routes/profile/partnerShip/partnerShipStep1.tsx
+++ src/Routes/profile/partnerShip/partnerShipStep1.tsx
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
-import CameraComponent from "../../../Component/camera/camera";
+import React, { useEffect, useRef, useState } from "react";
 import instanceMultipart from "../../../Component/axios/axiosMultipart";
 import { useAlert } from "../../../hook/useAlert/useAlert";
 import { useNavigate } from "react-router-dom";
+import { Camera } from "react-camera-pro";
+
+interface CameraComponent {
+  takePhoto: () => any; // Replace 'any' with the actual return type of 'takePhoto'
+}
 
 const PartnerShipStep1: React.FC = () => {
   const alertBox = useAlert();
   const navigate = useNavigate();
   const [photo, setPhoto] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
-
+  const camera = useRef<CameraComponent | null>(null);
   const handleCapture = (photo: string) => {
     setLoading(true);
     setPhoto(photo);
@@ -48,37 +52,53 @@ const PartnerShipStep1: React.FC = () => {
     return new Blob([ab], { type: mimeString });
   };
 
-  return (
-    <>
-      <div className="w-full h-screen bg-zinc-500 px-4 flex flex-col justify-start items-center">
-        <span className="text-white font-bold">신분증 인증</span>
-        {photo ? (
-          <div className="w-full h-auto relative rounded-lg">
-            <img src={photo} alt="Captured" />
-            <div className="absolute -left-1 -top-1 -bottom-1 border-4 rounded-full w-1 border-white "></div>
-            <div className="absolute -right-1 -top-1 -bottom-1 border-4 rounded-full w-1 border-white "></div>
-            <div className="absolute -right-1 -top-1 border-4  rounded-full w-8 border-white "></div>
-            <div className="absolute -right-1 -bottom-1 border-4 rounded-full w-8 border-white "></div>
-            <div className="absolute -left-1 -top-1 border-4 rounded-full w-8 border-white "></div>
-            <div className="absolute -left-1 -bottom-1 border-4 rounded-full w-8 border-white "></div>
-          </div>
-        ) : (
-          <CameraComponent onCapture={handleCapture} />
-        )}
+  const handleTakePhoto = () => {
+    if (camera.current) {
+      const photo = camera.current.takePhoto();
+      handleCapture(photo);
+    }
+  };
 
-        {!loading ? (
-          <div className="flex flex-col justify-center items-center text-center">
-            <span>영역 안에 신분증이 꽉 차도록 배치후</span>
-            <span>하단 버튼을 누르면 촬영됩니다.</span>
+  return (
+    <div className="w-full h-screen bg-zinc-500 px-4 flex flex-col justify-start items-center">
+      <span className="text-white font-bold">신분증 인증</span>
+      {photo ? (
+        <div className="w-full h-auto relative rounded-lg">
+          <img src={photo} alt="Captured" />
+          <div className="absolute -left-1 -top-1 -bottom-1 border-4 rounded-full w-1 border-white "></div>
+          <div className="absolute -right-1 -top-1 -bottom-1 border-4 rounded-full w-1 border-white "></div>
+          <div className="absolute -right-1 -top-1 border-4  rounded-full w-8 border-white "></div>
+          <div className="absolute -right-1 -bottom-1 border-4 rounded-full w-8 border-white "></div>
+          <div className="absolute -left-1 -top-1 border-4 rounded-full w-8 border-white "></div>
+          <div className="absolute -left-1 -bottom-1 border-4 rounded-full w-8 border-white "></div>
+        </div>
+      ) : (
+        <div className="w-full h-auto relative rounded-lg">
+          <Camera ref={camera} aspectRatio={16 / 9} facingMode="environment" errorMessages={{}} />
+          <div className="absolute -left-1 -top-1 -bottom-1 border-4 rounded-full w-1 border-white "></div>
+          <div className="absolute -right-1 -top-1 -bottom-1 border-4 rounded-full w-1 border-white "></div>
+          <div className="absolute -right-1 -top-1 border-4  rounded-full w-8 border-white "></div>
+          <div className="absolute -right-1 -bottom-1 border-4 rounded-full w-8 border-white "></div>
+          <div className="absolute -left-1 -top-1 border-4 rounded-full w-8 border-white "></div>
+          <div className="absolute -left-1 -bottom-1 border-4 rounded-full w-8 border-white "></div>
+          <div onClick={handleTakePhoto} className="bg-white w-16 h-16 rounded-full flex justify-center items-center">
+            <div className="bg-main w-14 h-14 rounded-full"></div>
           </div>
-        ) : (
-          <div className="flex flex-col justify-center items-center text-center">
-            <i className="xi-spinner-4 xi-spin xi-3x"></i>
-            <span>인식중</span>
-          </div>
-        )}
-      </div>
-    </>
+        </div>
+      )}
+
+      {!loading ? (
+        <div className="flex flex-col justify-center items-center text-center">
+          <span className="text-white font-bold">영역 안에 신분증이 꽉 차도록 배치후</span>
+          <span className="text-white font-bold">하단 버튼을 누르면 촬영됩니다.</span>
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center text-center">
+          <i className="xi-spinner-4 xi-spin xi-3x"></i>
+          <span>인식중</span>
+        </div>
+      )}
+    </div>
   );
 };
 
