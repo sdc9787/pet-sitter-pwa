dbd219ee5c52cbee7bbb3c24ea426acc32034333
sdc9787 <106896972+sdc9787@users.noreply.github.com>
2024-06-04 09:36:28 +0900

    test

--- src/Component/camera/camera.jsx
+++ src/Component/camera/camera.jsx
@@ -16,11 +16,11 @@ const CameraComponent = ({ onCapture }) => {
     <>
       <div className="w-full h-auto relative rounded-lg">
         <Camera className="rounded-lg" ref={camera} aspectRatio={16 / 9} facingMode="environment" />
-        <div className="absolute -left-1 -top-1 -bottom-1border-4 rounded-full w-1 border-white "></div>
+        <div className="absolute -left-1 -top-1 -bottom-1 border-4 rounded-full w-1 border-white "></div>
         <div className="absolute -right-1 -top-1 -bottom-1 border-4 rounded-full w-1 border-white "></div>
         <div className="absolute -right-1 -top-1 border-4  rounded-full w-6 border-white "></div>
         <div className="absolute -right-1 -bottom-1 border-4 rounded-full w-6 border-white "></div>
-        <div className="absolute -left-1 -top-1 -bottom-1 border-4 rounded-full w-6 border-white "></div>
+        <div className="absolute -left-1 -top-1 border-4 rounded-full w-6 border-white "></div>
         <div className="absolute -left-1 -bottom-1 border-4 rounded-full w-6 border-white "></div>
       </div>
       <button onClick={handleTakePhoto}>Take Photo</button>
